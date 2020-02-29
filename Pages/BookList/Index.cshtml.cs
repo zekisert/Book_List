291e6b96151cd8fcf54e5a9fe3d44a9e541a31ff
zekisert <zsert1963@gmail.com>
2020-02-29 20:16:50 +0300

    Create View

---
+++ Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+
+namespace BookListRazor.Pages.BookList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+            
+
+        public async Task OnGet()
+        {
+            Books = await _db.Books.ToListAsync();
+        }
+    }
+}

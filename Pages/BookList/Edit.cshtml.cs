104cc02fa4945ecabd7a935334a03978eceed640
zekisert <49846775+zekisert@users.noreply.github.com>
2020-02-29 22:28:45 +0300

    Create Edit.cshtml.cs

---
+++ Pages/BookList/Edit.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor
+{
+    public class EditModel : PageModel
+    {
+        private ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Books { get; set; }
+
+
+        public async Task OnGet(int id)
+        {
+            Books = await _db.Books.FindAsync(id);
+        }
+    }
+}
